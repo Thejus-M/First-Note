f4341a1f9d5bc648cb2302b6ca08b26b34081d56
Thejus <Git@tigerraw22.anonaddy.com>
2021-11-25 21:19:23 +0530

    created a small website with bootstrap

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
 @app.route('/')
 def hello():
-    return 'hello'
+    return render_template('index.html')
+
+@app.route('/products')
+def pro():
+    return 'products'
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # app.run(debug=True, port=8000)
